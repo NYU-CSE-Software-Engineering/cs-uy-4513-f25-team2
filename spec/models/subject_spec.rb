4fd27592086875e751ed2dd6b75f728db0f43682
EdwardK-GitHub <ek3588@nyu.edu>
2025-11-02 20:02:34 -0500

    Add failing model spec for subject code and name validation

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'is invalid without a name' do
+      s = Subject.new(code: 'MATH101')
+      expect(s).not_to be_valid
+      expect(s.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is invalid without a code' do
+      s = Subject.new(name: 'Calculus')
+      expect(s).not_to be_valid
+      expect(s.errors[:code]).to include("can't be blank")
+    end
+
+    it 'requires code to be unique (case-insensitive)' do
+      Subject.create!(name: 'Calculus', code: 'MATH101')
+      dup = Subject.new(name: 'Linear Algebra', code: 'math101')
+      expect(dup).not_to be_valid
+      expect(dup.errors[:code]).to include('has already been taken')
+    end
+  end
 end
