4872b43ee48b19139cf7ac7d48e7e5af52172789
EdwardK-GitHub <ek3588@nyu.edu>
2025-11-02 20:04:32 -0500

    Make subject code and name validation spec pass

--- app/models/learner.rb
+++ app/models/learner.rb
@@ -1,2 +1,5 @@
 class Learner < ApplicationRecord
+  validates :email,    presence: { message: 'is required' },
+                       uniqueness: { case_sensitive: false, message: 'has already been taken.' }
+  validates :password, presence: { message: 'is required' }
 end
