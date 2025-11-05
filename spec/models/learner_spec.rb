e7163de92c74f5dbf8a722eaac8560da86c8c73a
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-05 00:17:13 -0500

    Add failing model spec for learner unique email validation

--- spec/models/learner_spec.rb
+++ spec/models/learner_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Learner, type: :model do
   context 'validations' do
     it 'is invalid without an email' do
-      l = Learner.new(password: 'password')
+      l = Learner.new(password: "password")
       expect(l).not_to be_valid
       expect(l.errors[:email]).to include("can't be blank")
     end
@@ -12,6 +12,13 @@ RSpec.describe Learner, type: :model do
       l = Learner.new(email: "example@email.com")
       expect(l).not_to be_valid
       expect(l.errors[:password]).to include("can't be blank")
+    end
+
+    it 'enforces case-insensitive unique email' do
+      Learner.create!(email: "learner@email.com", password: "password")
+      dup = Learner.new(email: "LEARNER@email.com", password: "pass")
+      expect(dup).not_to be_valid
+      expect(dup.errors[:email]).to include('has already been taken')
     end
   end
 end
