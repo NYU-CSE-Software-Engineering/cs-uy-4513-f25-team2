80c52c661e3e0babcb12ca90b93ba220a659db60
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-05 21:57:33 -0500

    Add failing model spec for learner email validation

---
+++ spec/models/learner_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Learner, type: :model do
+  context 'validations' do
+    it 'is invalid without an email' do
+      l = Learner.new(password: "password")
+      expect(l).not_to be valid
+      expect(l.errors[:email]).to include("can't be blank")
+    end
+  end
+end
