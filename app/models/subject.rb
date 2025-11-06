58a913be10b6f433c10c7dd085ec0e4def9eacb6
alant9966 <alant9966@gmail.com>
2025-11-05 23:20:47 -0500

    Clean up associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,7 @@
 class Subject < ApplicationRecord
+  has_many :teaches, dependent: :destroy
+  has_many :tutors, through: :teaches
+
   validates :name, presence: true
   validates :code, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,5 +1,8 @@
 class Tutor < ApplicationRecord
     belongs_to :learner
+    has_many :teaches, dependent: :destroy
+    has_many :subjects, through: :teaches
+
     validates :learner, uniqueness: true
     validates :rating_avg, numericality: { 
         allow_nil: true, 
