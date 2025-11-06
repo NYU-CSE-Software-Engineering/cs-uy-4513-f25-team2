2ad64659e3826f5ab684ee9b35c0c8446ba46b44
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-05 22:02:38 -0500

    Make learner email validation spec pass

--- spec/models/learner_spec.rb
+++ spec/models/learner_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Learner, type: :model do
   context 'validations' do
     it 'is invalid without an email' do
       l = Learner.new(password: "password")
-      expect(l).not_to be valid
+      expect(l).not_to be_valid
       expect(l.errors[:email]).to include("can't be blank")
     end
   end
