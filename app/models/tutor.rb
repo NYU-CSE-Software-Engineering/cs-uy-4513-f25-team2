8422a0a9d223aa8fa6f5eb22e4df0c2c587c1f16
alant9966 <alant9966@gmail.com>
2025-11-05 22:05:00 -0500

    Make tutor average rating range validation spec pass

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,5 +1,9 @@
 class Tutor < ApplicationRecord
     belongs_to :learner
     validates :learner_id, uniqueness: true
-    validates :rating_avg, numericality: { allow_nil: true } 
+    validates :rating_avg, numericality: { 
+        allow_nil: true, 
+        greater_than_or_equal_to: 0, 
+        less_than_or_equal_to: 5 
+    }
 end
