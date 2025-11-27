b41a76b0a6ac6a8cade794ee995a7f721ef41a83
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-26 20:45:44 -0500

    Make TutorSession non-positive capacity validation spec pass

--- app/models/tutor_session.rb
+++ app/models/tutor_session.rb
@@ -1,8 +1,10 @@
 class TutorSession < ApplicationRecord
+  belongs_to :tutor
+  belongs_to :subject
+
   validates :tutor , presence: true
   validates :subject, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
-  belongs_to :tutor
-  belongs_to :subject
+  validates :capacity, presence: true, numericality: {greater_than: 0}
 end
