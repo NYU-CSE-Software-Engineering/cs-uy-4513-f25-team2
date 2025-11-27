bfd035539b73fb2d0ac5e896d781d5a7d922c1b1
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-26 21:45:12 -0500

    Make SessionAttendee overbooking validation spec pass

--- app/models/session_attendee.rb
+++ app/models/session_attendee.rb
@@ -4,4 +4,15 @@ class SessionAttendee < ApplicationRecord
 
   validates :tutor_session, presence: true
   validates :learner, presence: true
+  validate :session_not_full
+
+  def session_not_full
+    return unless tutor_session
+    # get the number of people already signed up for the session
+    attendee_count = SessionAttendee.where(tutor_session_id: tutor_session.id).count
+    # When a new SessionAttendee is being created, check if it there is over capacity
+    if new_record? && attendee_count >= tutor_session.capacity
+      errors.add(:base, "This session is full")
+    end
+  end
 end
