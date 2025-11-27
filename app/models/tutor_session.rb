603276b307f745fea8183ebda5cecf5658b6e3e8
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-11-26 20:31:13 -0500

    Make TutorSession start_at validation spec pass

--- app/models/tutor_session.rb
+++ app/models/tutor_session.rb
@@ -1,6 +1,7 @@
 class TutorSession < ApplicationRecord
   validates :tutor , presence: true
   validates :subject, presence: true
+  validates :start_at, presence: true
   belongs_to :tutor
   belongs_to :subject
 end
