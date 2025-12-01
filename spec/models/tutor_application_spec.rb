e6bbfe90259865592dd1fa182556f67c2b142fce
Alok-Dumet <156627711+Alok-Dumet@users.noreply.github.com>
2025-12-01 17:21:30 -0500

    Add failing model spec for the reason attribute of tutor applications

---
+++ spec/models/tutor_application_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe TutorApplication, type: :model do
+  context 'validations' do
+
+    it "is invalid without a reason" do
+      app = TutorApplication.new(reason: nil)
+
+      expect(app).not_to be_valid
+      expect(app.errors[:reason]).to include("can't be blank")
+    end
+
+  end
+end
