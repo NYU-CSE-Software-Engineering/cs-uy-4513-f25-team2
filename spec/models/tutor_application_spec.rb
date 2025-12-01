87c3e24d91ede5bc61c992796f9d0cd69f9735de
Alok-Dumet <156627711+Alok-Dumet@users.noreply.github.com>
2025-12-01 18:00:47 -0500

    Add failing model spec for presence of the status attribute of tutor applications

--- spec/models/tutor_application_spec.rb
+++ spec/models/tutor_application_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe TutorApplication, type: :model do
       expect(app.errors[:reason]).to include("can't be blank")
     end
 
+    it "is invalid without a status" do
+      app = TutorApplication.new(status: nil)
+      expect(app).not_to be_valid
+      expect(app.errors[:status]).to include("can't be blank")
+    end
+
   end
 end
