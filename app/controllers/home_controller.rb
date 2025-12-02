40ed4dd162aa72586314bdf1d2fbbfa3253c9994
Alok Aenugu <aenugualok@gmail.com>
2025-12-02 18:55:51 -0500

    Moved all GET tests from tutor_application_spec to home_spec to reflect change in logic. We now want to display status on the home page instead of a seperate application page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,16 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if current_learner
+
+      tutor_app = current_learner.tutor_application
+      @tutor_application_status =
+        if tutor_app.nil?
+          :none
+        elsif tutor_app.status == "pending"
+          :pending
+        elsif tutor_app.status == "approved"
+          :approved
+        end
+    end
+  end
 end
