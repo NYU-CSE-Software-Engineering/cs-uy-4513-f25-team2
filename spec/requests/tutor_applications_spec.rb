61e9ef54add958df798b7a129809c022c668df15
Alok Aenugu <aenugualok@gmail.com>
2025-12-02 17:10:44 -0500

    added failing spec for POST /tutor_applications to require a flash confirmation

--- spec/requests/tutor_applications_spec.rb
+++ spec/requests/tutor_applications_spec.rb
@@ -57,21 +57,27 @@ RSpec.describe "TutorApplications", type: :request do
 
 
   describe "POST /tutor_applications" do
-    it "creates a TutorApplication with the current learner, pending status, and submitted reason" do
-      # set the current logged-in user as this fake user we made
+    before do
       allow_any_instance_of(ApplicationController).to receive(:current_learner).and_return(learner)
-      post "/tutor_applications", params: {
-        tutor_application: { reason: "I like helping others" }
-      }
+    end
 
-      expect(TutorApplication.count).to eq(1)
-      expect(response).to render_template(:new)
+    context "when valid parameters are passed" do
+      let(:valid_params) do
+        { tutor_application: { reason: "I like helping others" } }
+      end
 
-      application = TutorApplication.last
+      it "creates a TutorApplication with pending status and redirects/rerenders correctly" do
+        expect {
+          post "/tutor_applications", params: valid_params
+        }.to change(TutorApplication, :count).by(1)
 
-      expect(application.learner_id).to eq(learner.id)
-      expect(application.status).to eq("pending")
-      expect(application.reason).to eq("I like helping others")
+        application = TutorApplication.last
+        expect(application.learner_id).to eq(learner.id)
+        expect(application.status).to eq("pending")
+        expect(application.reason).to eq("I like helping others")
+        expect(response).to render_template(:new)
+        expect(flash[:notice]).to eq("Application Sent!")
+      end
     end
   end
 end
